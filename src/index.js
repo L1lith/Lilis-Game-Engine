7af9c359847ad1cf3c8e6ae14ed26a2da67b8489
BuildTools <lily@l1lith.com>
2025-12-03 20:14:53 -0700

    add missing export

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import createGameCore from "./createGameCore";
 import createGameLoop from "./createGameLoop";
 import createEntityList from "./createEntityList";
 import createRenderSettings from "./createRenderSettings";
+import createEntity from "./createEntity";
 
 export const PixiRenderer = convertFunctionToConstructor(createPixiRenderer);
 export const GameLoop = convertFunctionToConstructor(createGameLoop);
 export const GameCore = convertFunctionToConstructor(createGameCore);
+export const Entity = convertFunctionToConstructor(createEntity);
 export const EntityList = convertFunctionToConstructor(createEntityList);
 export const RenderSettings =
   convertFunctionToConstructor(createRenderSettings);
