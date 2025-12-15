137fd9f84654afb429f7c9b1bd57b63364b6ea87
BuildTools <lily@l1lith.com>
2025-12-15 06:11:10 -0700

    strip out all the unnecessary bits

--- dev-setup/src/components/game.jsx
+++ dev-setup/src/components/game.jsx
@@ -1,20 +1,5 @@
-import "../styles/game.scss"
-import { onCleanup, onMount } from "solid-js"
-import runGame from "../game/runGame.js"
+import p5 from 'p5'
 
 export default function Game() {
-    let container = null
-    let stopGame = null
-
-    onMount(async ()=>{
-        const isClient = typeof window !== 'undefined'
-        if (!isClient) return
-        stopGame = await runGame(container)
-    })
-    onCleanup(()=>{
-        const isClient = typeof window !== 'undefined'
-        if (!isClient) return
-        stopGame()
-    })
-    return <div ref={container}/>
+    return <div />
 }
--- dev-setup/src/game/formats/RenderSettings.js
+++
@@ -1,9 +0,0 @@
-import { isStore } from "jabr";
-import InnerRenderSettingsFormat from "./InnerRenderSettings";
-import { valid } from "sandhands";
-
-export default {
-  _: Object,
-  validate: (renderSettings) =>
-    isStore(renderSettings) && valid(renderSettings, InnerRenderSettingsFormat),
-};
