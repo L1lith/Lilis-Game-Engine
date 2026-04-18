c8405e724ef0605957ac1e19788dbc5dd0f95c3d
BuildTools <lily@l1lith.com>
2026-04-17 23:55:18 -0600

    add entity defaults

--- src/createEntity.js
+++ src/createEntity.js
@@ -1,10 +1,21 @@
 import { Store, convertFunctionToConstructor } from "jabr";
 import Entity from "./formats/Entity.js";
 
-function createEntity(state) {
-  return new Store(state || {}, {
-    format: Entity,
-  });
+function createEntity(state = {}) {
+  const defaultEntity = {
+    x: 0,
+    y: 0,
+    z: 0,
+    width: 100,
+    height: 100,
+    depth: 100,
+  };
+  return new Store(
+    { ...defaultEntity, ...state },
+    {
+      format: Entity,
+    },
+  );
 }
 
 export default convertFunctionToConstructor(createEntity);
