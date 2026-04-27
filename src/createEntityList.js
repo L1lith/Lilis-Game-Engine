71db6b72a84bd0d393265baad9f2119043189b69
BuildTools <lily@l1lith.com>
2026-04-26 23:36:42 -0600

    remove sandhands type enforcement as it wasn't working and is untested, not necessarily permanent

--- src/createEntityList.js
+++ src/createEntityList.js
@@ -3,7 +3,7 @@ import { Signal } from "jabr";
 import InnerEntityListFormat from "./formats/InnerEntityList.js";
 
 function createEntityList(initialList = []) {
-  return new Signal(initialList, InnerEntityListFormat);
+  return new Signal(initialList /*, InnerEntityListFormat*/);
 }
 
 export default convertFunctionToConstructor(createEntityList);
