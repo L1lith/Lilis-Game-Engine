d057145e626f200f3c3fde3b3494d6f5102e3fec
BuildTools <lily@l1lith.com>
2026-04-26 22:07:41 -0600

    prepare for publish

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ export { default as Entity } from "./createEntity.js";
 export { default as EntityList } from "./createEntityList.js";
 export { default as GameCore } from "./createGameCore.js";
 export { default as RenderSettings } from "./createRenderSettings.js";
-export { default as Camera } from "./createCamera";
-export { default as CanvasManager } from "./canvasManager";
+export { default as Camera } from "./createCamera.js";
+export { default as CanvasManager } from "./canvasManager.js";
 
 // Functional Style Exports
 export { default as createGameLoop } from "./createGameLoop.js";
@@ -13,8 +13,5 @@ export { default as createEntity } from "./createEntity.js";
 export { default as createEntityList } from "./createEntityList.js";
 export { default as createGameCore } from "./createGameCore.js";
 export { default as createRenderSettings } from "./createRenderSettings.js";
-export { default as createCamera } from "./createCamera";
-export { default as createCanvasManager } from "./canvasManager";
-
-// Utilities
-export { default as translateToNewOrigin } from "./utility/translateToNewOrigin.js";
+export { default as createCamera } from "./createCamera.js";
+export { default as createCanvasManager } from "./canvasManager.js";
