f6c944e6efa4cdfa769b5b869d359955be4d9c59
BuildTools <lily@l1lith.com>
2026-06-25 23:23:32 -0600

    update the level loader

--- src/createLevelLoader.js
+++ src/createLevelLoader.js
@@ -1,10 +1,20 @@
-import { Signal, convertFunctionToConstructor } from "jabr";
-import EntityList from "./createEntityList.js";
+import { Signal, convertFunctionToConstructor, Store } from "jabr";
+import { EntityList } from "lilis-engine";
 
-async function createLevelLoader(entityList, levels, defaultLevel = null) {
+async function createLevelLoader(entityList, levels, options = {}) {
+  const {
+    defaultLevel = null,
+    globalContext: globalContextInput,
+    player = null,
+  } = options;
+  const globalContext =
+    typeof globalContextInput == "object" && globalContextInput !== null
+      ? globalContextInput
+      : {};
   const resolveLevel = (nameOrLevel) => {
     if (typeof nameOrLevel === "string") {
-      if (!(nameOrLevel in levels)) throw new Error("Unrecognized Level Name");
+      if (!(nameOrLevel in levels))
+        throw new Error("Unrecognized Level Name: " + nameOrLevel);
       if (
         typeof levels[nameOrLevel] != "object" ||
         levels[nameOrLevel] === null
@@ -38,22 +48,32 @@ async function createLevelLoader(entityList, levels, defaultLevel = null) {
     entityList.set(updatedEntityList);
   };
   const loadLevel = async (nameOrLevel) => {
-    console.log("Loading Level:", nameOrLevel);
+    let coords = null;
+    if (typeof nameOrLevel == "string" && nameOrLevel.includes("@")) {
+      const [newName, rawCoords] = nameOrLevel.split("@");
+      nameOrLevel = newName;
+      coords = rawCoords.split(",").map((value) => parseFloat(value));
+    }
     await unloadLevel();
     const level = resolveLevel(nameOrLevel);
     await mountLevel(level);
     activeLevel.set(level);
+    if (coords !== null && player !== null) {
+      player.x = coords[0];
+      player.y = coords[1];
+    }
     if (typeof level.load == "function") {
-      await level.load(level, loadLevel);
+      await level.load(globalContext, level);
     }
     level.entityList.addListener(entityListener);
     entityListener(level.entityList.get(), []);
   };
+  globalContext.load = loadLevel;
   const unloadLevel = async () => {
     const level = activeLevel.get();
     if (level === null) return; // No level is currently loaded
     if (typeof activeLevel.unload == "function")
-      await activeLevel.unload(level, loadLevel);
+      await activeLevel.unload(globalContext, level);
     activeLevel.set(null);
     level.entityList.removeListener(entityListener);
     entityListener([], level.entityList.get());
@@ -65,17 +85,25 @@ async function createLevelLoader(entityList, levels, defaultLevel = null) {
       typeof level.defaultEntities == "object" && level.defaultEntities !== null
         ? level.defaultEntities
         : new EntityList();
+    level.exports = new Store();
     if (typeof level.mount == "function") {
-      level.exports = (await level.mount(level, loadLevel)) || null;
+      const mountOutput = await level.mount(globalContext, level);
+      if (typeof mountOutput == "object" && mountOutput !== null) {
+        Object.keys(mountOutput).forEach((key) => {
+          console.log("assigning", typeof key, key, mountOutput[key]);
+          level.exports[key] = mountOutput[key];
+        });
+      }
     }
     mountedLevels = mountedLevels.concat([level]);
     level.isMounted = true;
   };
+  globalContext.preload = mountLevel;
   const unmountLevel = async (level) => {
     level = resolveLevel(level);
     if (!mountedLevels.includes(level)) return;
     if (typeof level.unmount == "function") {
-      await level.unmount(level, loadLevel);
+      await level.unmount(globalContext, level);
       level.exports = null;
     }
     mountedLevels = mountedLevels.filter((matchLevel) => matchLevel !== level);
@@ -87,10 +115,10 @@ async function createLevelLoader(entityList, levels, defaultLevel = null) {
     await unloadLevel();
     await Promise.all(mountedLevels.map((level) => unmountLevel(level)));
   };
-  const tick = async () => {
+  const tick = async (...args) => {
     const level = activeLevel.get();
     if (level && typeof level.tick == "function")
-      await level.tick(level, loadLevel);
+      await level.tick(globalContext, level, ...args);
   };
   return {
     mount,
@@ -100,6 +128,7 @@ async function createLevelLoader(entityList, levels, defaultLevel = null) {
     loadLevel,
     unloadLevel,
     activeLevel,
+    globalContext,
   };
 }
 
