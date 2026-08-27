1b712781dd7f193f8060d956196110e84c329e56
BuildTools <lily@l1lith.com>
2026-08-27 16:17:35 -0600

    get the level loader example working better

---
+++ examples/level-loader-demo/src/components/Game.jsx
@@ -0,0 +1,25 @@
+import { onMount, onCleanup} from "solid-js"
+import { isServer } from 'solid-js/web'
+import {createGameCore, Entity, EntityList, RenderSettings, createGameLoop} from 'lilis-engine'
+import createPixiRenderer from 'lilis-engine/pixi'
+
+export default function Game() {
+    let canvas
+    let unmountGameEngine
+    onMount(async ()=>{
+        if (isServer) return
+        console.log('Mounted! Rendering a chicken on the canvas every animation frame')
+        const renderSettings = new RenderSettings({canvas})
+        const player = new Entity({imageURL: '/chicken by Diarandor.png', x: 0, y: 0, width: 50, height: 50})
+        const entities = new EntityList([player])
+        const pixiRenderer = createPixiRenderer(entities, renderSettings)
+        const gameCore = createGameCore({plugins:[createGameLoop(), pixiRenderer]})
+        await gameCore.mount()
+        unmountGameEngine = gameCore.unmount
+    })
+    onCleanup(async ()=>{
+        if (isServer) return // Browser Only, shut down the game engine. Not technically mandatory but good practice and shows how to gracefully shut down the game engine
+        await unmountGameEngine()
+    })
+    return (<canvas ref={canvas}/>)
+}
