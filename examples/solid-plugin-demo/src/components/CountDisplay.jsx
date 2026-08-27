cf772a0f1acc272c048368efbe6225cc026bd53c
BuildTools <lily@l1lith.com>
2026-08-26 19:49:36 -0600

    add a regular signal for comparison

--- examples/solid-plugin-demo/src/components/CountDisplay.jsx
+++ examples/solid-plugin-demo/src/components/CountDisplay.jsx
@@ -1,3 +1,11 @@
+import { createSignal, onMount  } from "solid-js"
+import { } from "solid-js"
 export default function TestText({getReactiveProp}) {
-    return <p>Time Passed: {getReactiveProp('count')} seconds</p>
+    const [regular, setRegular] = createSignal(0)
+    onMount(()=>{
+        setInterval(()=>{
+            setRegular(regular() + 1)
+        }, 1000)
+    })
+    return <p>Time Passed: {getReactiveProp('count')} seconds, regular: {regular()}</p>
 }
