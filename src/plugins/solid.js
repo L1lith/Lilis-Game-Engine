a8e8b0e25c51f39bc1024e99450ac42275051745
BuildTools <lily@l1lith.com>
2026-08-26 19:15:45 -0600

    add getReactiveProp helper

--- src/plugins/solid.js
+++ src/plugins/solid.js
@@ -22,7 +22,11 @@ function createSolidRenderer(entities, renderSettings) {
     if (typeof entity.solid == "function") {
       // Wrap the .solid property with the entity as a prop and optionally deep pass all the properties of it
       const SolidComponent = entity.solid;
-      entity._wrappedComponent = untrack(() => SolidComponent({ entity }));
+      const getReactiveProp = (propName) =>
+        createSolidGetter(entity.getSignal(propName));
+      entity._wrappedComponent = untrack(() =>
+        SolidComponent({ entity, getReactiveProp }),
+      );
       // entity._wrappedComponent = _$createComponent(SolidComponent, {
       //   entity: entity,
       // });
