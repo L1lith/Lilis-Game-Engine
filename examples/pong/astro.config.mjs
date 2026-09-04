7d1fdeac33ef9027c237bd204c1ae87784115f6b
BuildTools <lily@l1lith.com>
2026-09-03 23:36:55 -0600

    update the workflow for performance reasons

--- examples/pong/astro.config.mjs
+++ examples/pong/astro.config.mjs
@@ -7,6 +7,6 @@ import solidJs from "@astrojs/solid-js";
 export default defineConfig({
   integrations: [solidJs()],
   site: "https://l1lith.github.io",
-  base: "/Lilis-Game-Engine/pong", // This is the subpath
+  base: "/Lilis-Game-Engine/examples/pong", // This is the subpath
   output: "static", // SSG mode
 });
