7b38262e6c7f456094baa83e16b3c5330f4e858e
BuildTools <lily@l1lith.com>
2026-09-03 20:24:44 -0600

    improve table split and font

--- examples/pong/src/components/Pong.jsx
+++ examples/pong/src/components/Pong.jsx
@@ -8,13 +8,13 @@ import {
   createGameLoop,
   createEntityList,
 } from "lilis-engine";
-import {detectKeys} from 'lilis-engine/utility'
+import { detectKeys } from "lilis-engine/utility";
 import createMatterPlugin from "lilis-engine/matter";
 import createP5Renderer from "lilis-engine/p5";
-import {Signal} from 'jabr'
+import { Signal } from "jabr";
 import Matter from "matter-js";
-Matter.Resolver._restingThresh = 0.001
-const {Body} = Matter
+Matter.Resolver._restingThresh = 0.001;
+const { Body } = Matter;
 
 export default function Pong() {
   let canvas;
@@ -41,21 +41,36 @@ export default function Pong() {
         width: 100,
         height: 100,
         fill: "#000000",
-        renderPriority: -100
+        renderPriority: -100,
       }),
     );
-    const infoColor = '#8e8e8e'
-       const tableSplit = entities.addChild(Entity({
-        color: 'white',
+    const infoColor = "#8e8e8e";
+    const tableSplit = entities.addChild(
+      Entity({
+        color: "white",
         stroke: infoColor,
-        render: p=>{
-            p.drawingContext.setLineDash([5, 10, 30, 10])
-            p.strokeWeight(4)
-            p.line(p.width / 2, 0, p.width / 2, p.height)
-            p.strokeWeight(0)
+        render: (p) => {
+          p.drawingContext.setLineDash([5, 10, 30, 10]);
+          p.strokeWeight(4);
+          p.line(p.width / 2, 0, p.width / 2, p.height * .45);
+          p.strokeWeight(0);
         },
-        renderPriority: -10
-    }))
+        renderPriority: -10,
+      }),
+    );
+    const tableSplit2 = entities.addChild(
+      Entity({
+        color: "white",
+        stroke: infoColor,
+        render: (p) => {
+          p.drawingContext.setLineDash([5, 10, 30, 10]);
+          p.strokeWeight(4);
+          p.line(p.width / 2, p.height * .55, p.width / 2, p.height);
+          p.strokeWeight(0);
+        },
+        renderPriority: -10,
+      }),
+    );
     const leftPaddle = entities.addChild(
       Entity({
         shape: "rect",
@@ -91,7 +106,7 @@ export default function Pong() {
       }),
     );
 
-    const targetSpeed = Signal(1)
+    const targetSpeed = Signal(1);
     const ball = entities.addChild(
       Entity({
         shape: "ellipse",
@@ -101,7 +116,7 @@ export default function Pong() {
           restitution: 1,
           frictionAir: 0,
           //mass: 1,
-          inertia: Infinity
+          inertia: Infinity,
         },
         x: 0,
         y: 0,
@@ -117,7 +132,7 @@ export default function Pong() {
           static: true,
           restitution: 1,
           friction: 0,
-          mass: 1
+          mass: 1,
         },
         x: 0,
         y: -51,
@@ -126,7 +141,7 @@ export default function Pong() {
       }),
     );
 
-    const bottomWall = window.bottomWall = entities.addChild(
+    const bottomWall = (window.bottomWall = entities.addChild(
       Entity({
         matter: {
           shape: "rectangle",
@@ -134,14 +149,14 @@ export default function Pong() {
           restitution: 1,
           friction: 0,
           mass: 1,
-          inertia: Infinity
+          inertia: Infinity,
         },
         x: 0,
         y: 54,
         width: 100,
         height: 8,
       }),
-    );
+    ));
 
     const matterPlugin = createMatterPlugin(entities, {
       setup: (engine) => {
@@ -149,82 +164,122 @@ export default function Pong() {
         engine.gravity.y = 0;
         leftPaddle.y = 0;
         rightPaddle.y = 0;
-        engine.velocityIterations = 20
+        engine.velocityIterations = 20;
       },
     });
-    const player1Score = Signal(0)
-    const player2Score = Signal(0)
+    const player1Score = Signal(0);
+    const player2Score = Signal(0);
     const handlePlayerScore = (winner) => {
-        const targetScore = (winner === 1 ? player1Score : player2Score)
-        targetScore.set(targetScore.get() + 1)
-        console.log(`Player ${winner} scored!, total score: ${targetScore.get()}`)
-    }
+      const targetScore = winner === 1 ? player1Score : player2Score;
+      targetScore.set(targetScore.get() + 1);
+      console.log(
+        `Player ${winner} scored!, total score: ${targetScore.get()}`,
+      );
+    };
 
-    const player1ScoreDisplay = entities.addChild(Entity({
+    const player1ScoreDisplay = entities.addChild(
+      Entity({
         fill: infoColor,
-        render: (p, positionData)=>{
-            const {x, y} = positionData
-            p.textSize(p.width / 10)
-            p.textAlign(p.CENTER)
-            p.text(player1Score.get(), x, y)
+        render: (p, positionData) => {
+          const { x, y } = positionData;
+          p.textSize(p.width / 10);
+          p.textAlign(p.CENTER);
+          p.text(player1Score.get(), x, y);
         },
-        x: -25, y: -40,
-        renderPriority: -10
-    }))
-    const player2ScoreDisplay = entities.addChild(Entity({
+        x: -25,
+        y: -40,
+        renderPriority: -10,
+      }),
+    );
+    const player2ScoreDisplay = entities.addChild(
+      Entity({
         fill: infoColor,
-        render: (p, positionData)=>{
-            const {x, y} = positionData
-            p.textSize(p.width / 10)
-            p.textAlign(p.CENTER)
-            p.text(player2Score.get(), x, y)
+        render: (p, positionData) => {
+          const { x, y } = positionData;
+          p.textFont("Courier New");
+          p.textSize(p.width / 10);
+          p.textAlign(p.CENTER);
+          p.text(player2Score.get(), x, y);
         },
-        x: 25, y: -40,
-        renderPriority: -10
-    }))
+        x: 25,
+        y: -40,
+        renderPriority: -10,
+      }),
+    );
 
-    const ballInitialForce = 0.000025
-    const ballSpeed = 1.1
+    const ballInitialForce = 0.000025;
+    const ballSpeed = 1.1;
 
     const ballManager = {
-        tick: ()=>{
-            const isOffscreenLeft = ball.x < -50
-            const isOffscreenRight = ball.x > 50
-            if (isOffscreenLeft || isOffscreenRight) {
-                ball.x = 0
-                ball.y = 0
-                handlePlayerScore(isOffscreenLeft ? 2 : 1)
-            }
-            if (Body.getSpeed(ball.matterBody) === 0 || isOffscreenLeft || isOffscreenRight) {
-                const xForce = ball.matterBody.force.x = (ballInitialForce / 2 * Math.random() + ballInitialForce / 2) * (Math.random() < 0.5 ? -1 : 1)
-                ball.matterBody.force.y = Math.min((ballInitialForce / 2) * Math.random(), ballInitialForce / 2, Math.abs(xForce)) * (Math.random() < 0.5 ? -1 : 1) // Math.min here prevents us from shooting upwards balls which take forever to score.
-                
-                //Body.setVelocity(ball.matterBody, {x: 0.5 * Math.random() + 0.3, y: 0 /*0.5 * Math.random()*/})
-            } else {
-
-            }// else if (Body.getSpeed(ball.matterBody) < 0.5) {
-            Body.setSpeed(ball.matterBody, ballSpeed)
-            //     //Body.setSpeed(ball.matterBody, 1)
-            // }
-            // console.log(Body.getSpeed(ball.matterBody))
-           //Body.setSpeed(ball.matterBody, targetSpeed.get())
+      tick: () => {
+        const isOffscreenLeft = ball.x < -50;
+        const isOffscreenRight = ball.x > 50;
+        if (isOffscreenLeft || isOffscreenRight) {
+          ball.x = 0;
+          ball.y = 0;
+          handlePlayerScore(isOffscreenLeft ? 2 : 1);
         }
-    }
+        if (
+          Body.getSpeed(ball.matterBody) === 0 ||
+          isOffscreenLeft ||
+          isOffscreenRight
+        ) {
+          const xForce = (ball.matterBody.force.x =
+            ((ballInitialForce / 2) * Math.random() + ballInitialForce / 2) *
+            (Math.random() < 0.5 ? -1 : 1));
+          ball.matterBody.force.y =
+            Math.min(
+              (ballInitialForce / 2) * Math.random(),
+              ballInitialForce / 2,
+              Math.abs(xForce),
+            ) * (Math.random() < 0.5 ? -1 : 1); // Math.min here prevents us from shooting upwards balls which take forever to score.
 
-    const player1Controls = {up: detectKeys('w'), down: detectKeys('s')}
-    const player2Controls = {up: detectKeys('ArrowUp'), down: detectKeys('ArrowDown')}
+          //Body.setVelocity(ball.matterBody, {x: 0.5 * Math.random() + 0.3, y: 0 /*0.5 * Math.random()*/})
+        } else {
+        } // else if (Body.getSpeed(ball.matterBody) < 0.5) {
+        Body.setSpeed(ball.matterBody, ballSpeed);
+        //     //Body.setSpeed(ball.matterBody, 1)
+        // }
+        // console.log(Body.getSpeed(ball.matterBody))
+        //Body.setSpeed(ball.matterBody, targetSpeed.get())
+      },
+    };
 
-    const playerSpeed = 2
-    const enforceBounds = (paddleY, entity) => Math.min(50 - entity.height / 2, Math.max(-50 + entity.height / 2, paddleY))
+    const player1Controls = { up: detectKeys("w"), down: detectKeys("s") };
+    const player2Controls = {
+      up: detectKeys("ArrowUp"),
+      down: detectKeys("ArrowDown"),
+    };
+
+    const playerSpeed = 2;
+    const enforceBounds = (paddleY, entity) =>
+      Math.min(
+        50 - entity.height / 2,
+        Math.max(-50 + entity.height / 2, paddleY),
+      );
     const playerController = {
-        tick: ()=>{
-            const player1Direction = player1Controls.up.get() ? -1 : player1Controls.down.get() ? 1 : 0
-            const player2Direction = player2Controls.up.get() ? -1 : player2Controls.down.get() ? 1 : 0
-            leftPaddle.y = enforceBounds(leftPaddle.y + player1Direction * playerSpeed, leftPaddle)
-            rightPaddle.y = enforceBounds(rightPaddle.y + player2Direction * playerSpeed, rightPaddle)
-        }
-    }
-    
+      tick: () => {
+        const player1Direction = player1Controls.up.get()
+          ? -1
+          : player1Controls.down.get()
+            ? 1
+            : 0;
+        const player2Direction = player2Controls.up.get()
+          ? -1
+          : player2Controls.down.get()
+            ? 1
+            : 0;
+        leftPaddle.y = enforceBounds(
+          leftPaddle.y + player1Direction * playerSpeed,
+          leftPaddle,
+        );
+        rightPaddle.y = enforceBounds(
+          rightPaddle.y + player2Direction * playerSpeed,
+          rightPaddle,
+        );
+      },
+    };
+
     // End of main game setup
     const gameCore = createGameCore({
       plugins: [
@@ -232,7 +287,7 @@ export default function Pong() {
         createP5Renderer(entities, renderSettings),
         matterPlugin,
         ballManager,
-        playerController
+        playerController,
       ],
     });
     await gameCore.mount();
