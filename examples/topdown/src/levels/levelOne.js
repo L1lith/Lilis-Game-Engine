4f8d1351c5e0486a38b348d50ae2898376b0fea3
BuildTools <lily@l1lith.com>
2026-09-19 06:57:13 -0600

    add overhead layers to the topdown demo

--- examples/topdown/src/levels/levelOne.js
+++ examples/topdown/src/levels/levelOne.js
@@ -1,7 +1,8 @@
 import createPixiTiledmap from "lilis-engine/pixi-tiled";
 import { createMatterBoundaries } from "lilis-engine/matter";
 import pixiTiledToMatter from "lilis-engine/pixi-tiled-matter";
-import { Assets } from "pixi.js";
+import { Assets, RenderLayer } from "pixi.js";
+import { Entity } from "lilis-engine";
 
 export default {
   mount: async ({}, { entityList }) => {
@@ -12,7 +13,27 @@ export default {
       width: map.width,
       height: map.height,
     });
-    entityList.set([map, mapCollision, boundaries]);
+    const overhead = new Entity({
+      x: 0,
+      y: 0,
+      width: 100,
+      height: 100,
+      sprite: new RenderLayer(),
+    });
+    const overheadLayers = map.sprite.children.filter(
+      (layer) =>
+        Array.isArray(layer?.layerData.properties) &&
+        layer.layerData.properties.find(
+          (property) =>
+            property?.name === "overhead" && property?.value === true,
+        ),
+    );
+    overheadLayers.forEach((overheadLayer) =>
+      overhead.sprite.attach(overheadLayer),
+    );
+
+    overhead.renderPriority = 100;
+    entityList.set([overhead, map, mapCollision, boundaries]);
     return { map };
   },
 };
