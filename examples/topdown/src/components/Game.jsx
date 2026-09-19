a5834f1f30e768f81f211a530ab918f870526c09
BuildTools <lily@l1lith.com>
2026-09-19 06:50:24 -0600

    improve the topdown demo

--- examples/topdown/src/components/Game.jsx
+++ examples/topdown/src/components/Game.jsx
@@ -27,13 +27,21 @@ export default function Game() {
         };
         window.addEventListener("resize", autoResize);
         autoResize();
-        const playerCam = renderSettings.camera = Camera({width: 60, height: 60})
+        const playerCam = renderSettings.camera = Camera({width: 70, height: 70})
         window.cam = playerCam
         const entities = EntityList([])
         // GUI Stuff
         const touchControls = entities.addChild({solid: TouchControls})
         // End GUI Stuff
-        const player = window.player = entities.addChild({renderPriority: 3, x: 10, y:-30, width: 5, height: 5, matter: {shape: 'circle'}, imageURL: 'chicken by Diarandor.png'})
+        const player = window.player = entities.addChild({
+            renderPriority: 3, 
+            x: 10, y:-30, 
+            width: 5, 
+            height: 5, 
+            matter: {shape: 'circle'}, 
+            imageURL: 'chicken by Diarandor.png',
+            renderRotation: 0
+        })
         player.on('x', x => playerCam.x = x)
         player.on('y', y => playerCam.y = y)
         playerCam.x = player.x
@@ -50,10 +58,10 @@ export default function Game() {
         const playerControlPlugin = {
             tick: () => {
                 if (!player.matterBody) return
-                const xForce = inputs.right.get() || touchControls.direction === "right" ? (inputs.left.get() || touchControls.direction === "left" ? 0 : 1) : inputs.left.get() || touchControls.direction === "left" ? -1 : 0
-                const isTouchingSurface = (player.collisions || []).filter(event => event.colliderBody && event.colliderBody.position.y + event.colliderBody.bounds.max.y / 2 > player.y + player.height / 2 && (!event.colliderEntity || typeof event.colliderEntity.boundaryType != 'string')).length > 0
-                const isJumping = (inputs.up.get() || touchControls.jumping || inputs.space.get()) && isTouchingSurface //&& Body.getVelocity(player.matterBody).y < 0.001
-                Body.setVelocity(player.matterBody, {x: xForce * walkForce, y: isJumping ? jumpForce * -1 : player.matterBody.velocity.y})
+                const xForce = inputs.right.get() || touchControls.xDirection === "right" ? (inputs.left.get() || touchControls.xDirection === "left" ? 0 : 1) : inputs.left.get() || touchControls.direction === "left" ? -1 : 0
+                const yForce = inputs.down.get() || touchControls.yDirection === "down" ? (inputs.up.get() || touchControls.yDirection === "up" ? 0 : 1) : inputs.up.get() || touchControls.direction === "up" ? -1 : 0
+                //const isJumping = (inputs.up.get() || touchControls.jumping || inputs.space.get()) && isTouchingSurface //&& Body.getVelocity(player.matterBody).y < 0.001
+                Body.setVelocity(player.matterBody, {x: xForce * walkForce, y: yForce * walkForce})
             }
         }
         window.entities = entities
--- examples/topdown/src/levels/levelOne.js
+++ examples/topdown/src/levels/levelOne.js
@@ -11,7 +11,6 @@ export default {
     const boundaries = createMatterBoundaries({
       width: map.width,
       height: map.height,
-      skipBoundaries: ["bottom"],
     });
     entityList.set([map, mapCollision, boundaries]);
     return { map };
