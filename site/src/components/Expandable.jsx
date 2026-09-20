c7d59c1aab668828595233dbf83dd5eb9651cdd5
BuildTools <lily@l1lith.com>
2026-09-20 10:05:23 -0600

    improve the demo page scaling

--- site/src/components/Expandable.jsx
+++ site/src/components/Expandable.jsx
@@ -4,10 +4,9 @@ import '@/styles/Expandable.scss'
 export default function Expandable(props) {
     const {class: className, title="Untitled", children, startExpanded=false} = props
     const [expanded, setExpanded] = createSignal(startExpanded)
-    console.log(children, expanded)
     const toggleExpanded = ()=>{
         setExpanded(!expanded())
-        console.log(expanded())
+        console.log('expanded:', expanded())
     }
     return <div class={"expandable" + (typeof className == 'string' ? ' ' + className : '')}>
         <h2 class="title">{title}<button onClick={toggleExpanded}><span class="text-icon">{!expanded() ? '+' : '-'}</span></button></h2>
