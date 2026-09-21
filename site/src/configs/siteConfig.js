baff52af95738d52eb2657d491137fe81d486945
BuildTools <lily@l1lith.com>
2026-09-21 09:54:17 -0600

    add favicons

--- site/src/configs/siteConfig.js
+++ site/src/configs/siteConfig.js
@@ -1,1 +1,8 @@
-export default {};
+const faviconSizes = [16, 32, 48, 180, 192];
+
+export default {
+  favicons: faviconSizes.map(
+    (size) =>
+      `${import.meta.env.BASE_URL}favicons/lilis-game-engine-favicon-${size}.png`,
+  ),
+};
